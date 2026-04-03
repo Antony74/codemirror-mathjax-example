e77147c64f4dcab9af1491fbe36a0780fb635baa
Antony74 <akb@akb.me.uk>
2026-04-03 20:49:34 +0100

    Begin a textarea fallback

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { basicSetup } from 'codemirror';
 import { EditorView } from '@codemirror/view';
 
+const editor = document.getElementById('editor');
+
+if (!editor) {
+    throw new Error('Editor element not found');
+}
+
 const view = new EditorView({
     doc: 'Start document',
-    parent: document.body,
     extensions: [basicSetup],
+    parent: document.body,
 });
+
+// editor.remove();
+// view.dom.id = 'editor';
